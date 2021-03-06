fbd556ccc69209101eaafc397c798e5a1d0e7790
Thiago2436 <54513342+Thiago2436@users.noreply.github.com>
2021-03-06 17:19:37 -0300

    Primeiro comit
    somente o basico formulario e o html css

---
+++ formulario/cad.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<link rel="stylesheet" href="estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Form </title>
+</head>
+<body> 
+<?php
+echo " deu certo "
+
+?>
+</body>
+</html>
