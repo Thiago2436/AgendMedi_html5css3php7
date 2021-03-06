4aa81251d715b6cbb9c9a8c870300f8901d81ee3
Thiago2436 <54513342+Thiago2436@users.noreply.github.com>
2021-03-06 19:42:54 -0300

    Relatorio
    Ultima atualização de hj o Formulario gera relatorio do agendamento.

--- formulario/cad.php
+++ formulario/cad.php
@@ -7,7 +7,23 @@
 </head>
 <body> 
 <?php
-echo " deu certo "
+$cpf =$_POST['CPF'];
+$nome=$_POST['nome'];
+$nomemed=$_POST['NomeMed'];
+$esp=$_POST['EspecialidadeMed'];
+$Conv=$_POST['Convenio'];
+$Valor=$_POST['ValorCons'];
+$Data=$_POST['date'];
+$Hora=$_POST['hora'];
+echo " Resumo da Consulta ";
+echo "Nome : ".$nome."<br>";
+echo " Cpf : ".$cpf."<br>";
+echo " Nome do Medico : ".$nomemed."<br>";
+echo " Especialidade : ".$esp."<br>"; 
+echo " Convenio : ".$Conv."<br>";
+echo " Valor da Consulta : ".$Valor."<br>";
+echo " Data : ".$Data."<br>";
+echo " Hora : ".$Hora."<br>";
 
 ?>
 </body>
